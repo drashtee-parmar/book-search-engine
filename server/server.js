6f8ae97991275462ab3c299fc310199ca59da4df
Drashtee <drashteeparmar@gmail.com>
2022-04-02 22:03:02 -0400

    updated auth middleware and apollo server to server.js file

--- server/server.js
+++ server/server.js
@@ -1,10 +1,16 @@
 const express = require('express');
 const path = require('path');
+const { authMiddleware } = require('./utils/auth');
 const db = require('./config/connection');
 const routes = require('./routes');
 
-const app = express();
 const PORT = process.env.PORT || 3001;
+const app = express();
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+  context: authMiddleware,
+});
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
@@ -17,5 +23,8 @@ if (process.env.NODE_ENV === 'production') {
 app.use(routes);
 
 db.once('open', () => {
-  app.listen(PORT, () => console.log(`🌍 Now listening on localhost:${PORT}`));
+  app.listen(PORT, () => {
+    console.log(`API server running on port ${PORT}!`);
+    console.log(`Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`);
+  });
 });
