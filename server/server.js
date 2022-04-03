adc6f240ceb32fd35ff644180c92f8816b2b43ea
Drashtee <drashteeparmar@gmail.com>
2022-04-02 22:11:05 -0400

    Export your typeDefs and resolvers - Schema - index

--- server/server.js
+++ server/server.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const path = require('path');
+
+const { typeDefs, resolvers } = require('./schemas');
 const { authMiddleware } = require('./utils/auth');
 const db = require('./config/connection');
 const routes = require('./routes');
